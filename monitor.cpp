294645536fd7d1464375ed8585d02f47c4dcf2fe
Josef Sunesson <josefsunesson@n151-p239.kthopen.kth.se>
2013-03-05 14:04:25 +0100

    Add implementation of constructors and assignment operators in monitor.

--- monitor.cpp
+++ monitor.cpp
@@ -1,22 +1,40 @@
 #include "monitor.h"
+#include <vector>
 
-monitor::monitor()
+monitor::monitor(int number_of_elevators) : elevators(number_of_elevators)
 {
 }
-monitor::monitor(const monitor & source)
+
+monitor::monitor(const monitor & source) : elevators(source.elevators)
 {
 }
-monitor::monitor(monitor && source)
+
+monitor::monitor(monitor && source) : elevators(source.elevators)
 {
 }
+
 monitor::~monitor()
 {
 }
+
 monitor & monitor::operator=(const monitor & source)
 {
+    if (this != &source)
+    {
+        elevators = source.elevators;
+    }
     return *this;
 }
+
+monitor & monitor::operator=(monitor && source)
+{
+    if (this != &source)
+    {
+        elevators = source.elevators;
+    }
+    return *this;
+}
+
 void monitor::update_position(int elevator, double position)
 {
 }
-
