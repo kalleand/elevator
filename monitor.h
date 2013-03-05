d1d43760f0751bbba9e4ada46a6d10ae4bfe1056
Josef Sunesson <josefsunesson@n151-p239.kthopen.kth.se>
2013-03-05 13:54:20 +0100

    Added move-assignment operator and introduced a private vector of elevators.

--- monitor.h
+++ monitor.h
@@ -1,19 +1,24 @@
 #ifndef __ELEVATOR_MONITOR
 #define __ELEVATOR_MONITOR
 
+#include <vector>
+
 class monitor
 {
     public:
-        monitor();
+        monitor(int number_of_elevators);
         monitor(const monitor & source);
         monitor(monitor && source);
         ~monitor();
         monitor & operator=(const monitor & source);
+        monitor & operator=(monitor && source);
 
         // We need to read position
         void update_position(int elevator, double position);
         // Additional functions..
         // TODO
     private:
+        // PLEASE NOTE THAT WE DO NOT USE ELEVATOR 0!
+        std::vector<elevator> elevators;
 };
 #endif
