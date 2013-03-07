b87376fbaf29f2af66dcf0312b6ba0ee74c14311
Karl Johan Andreasson <kalleand@kth.se>
2013-03-07 15:12:57 +0100

    Added command constructors.

--- command.cpp
+++ command.cpp
@@ -1,2 +1,31 @@
 #include "command.h"
 
+command::command() : type(), desc()
+{
+}
+
+command::command(EventType new_type, EventDesc new_desc) : type(new_type), desc(new_desc)
+{
+}
+
+command::command(const command & source) : type(source.type), desc(source.desc)
+{
+}
+
+command::command(command && source) : type(source.type), desc(source.desc)
+{
+}
+
+command & command::operator=(const command & source)
+{
+    type = source.type;
+    desc = source.desc;
+    return *this;
+}
+
+command & command::operator=(command && source)
+{
+    type = source.type;
+    desc = source.desc;
+    return *this;
+}
