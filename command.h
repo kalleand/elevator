c99778b9b7966cacdb6760ed7f0487c8c5418a61
Josef Sunesson <josefsunesson@n151-p239.kthopen.kth.se>
2013-03-09 12:18:55 +0100

    Added comments.

--- command.h
+++ command.h
@@ -3,16 +3,29 @@
 
 #include "hardwareAPI.h"
 
+/*
+ * command is a small container class that groups together an EventType with the corresponding EventDesc description.
+ */
 class command
 {
     public:
+        /*
+         * Constructors
+         */
         command();
         command(EventType new_type, EventDesc new_desc);
         command(const command & source);
         command(command && source);
 
+        /*
+         * Assignment operators
+         */
         command & operator=(const command & source);
         command & operator=(command && source);
+
+        /*
+         * The EventType and EventDesc as public members
+         */
         EventType type;
         EventDesc desc;
 };
